f7385146900c0a52201bd99bde0223f777e95ada
wga1985 <wga1985@163.com>
2015-07-07 23:06:58 +0800

    add bsp systick usart1

--- APP/main.c
+++ APP/main.c
@@ -2,7 +2,23 @@
 
 /*-------------------------------------------------------------------------------------------------------*/
 
-int main(void)
+void Usart1_RxCallback_Func(unsigned char *buf, unsigned int len)
 {
+	printf("[Usart1_RxCallback_Func] len: %d, buf: %s\r\n", len, buf);
+}
+
+/*-------------------------------------------------------------------------------------------------------*/
+
+int main(void)
+{	
+	BSP_Init();	
+	usart1_rx_callback_register(Usart1_RxCallback_Func);
+	
+	while(1)
+	{
+		delay_ms(100);
+		printf("------main_loop------\r\n");
+	}
+	
 	return 0;
 }
